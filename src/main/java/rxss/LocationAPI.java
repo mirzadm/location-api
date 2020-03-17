b81999f8d77e89c8f808807a5c2bd9b755b08cda
Mirzad Mohandespour <12888868+mirzadm@users.noreply.github.com>
2020-03-17 17:43:30 -0500

    Rename app file

--- src/main/java/rxss/LocationAPI.java
+++
@@ -1,5 +0,0 @@
-package rxss;
-
-public class LocationAPI {
-
-}
---
+++ src/main/java/rxss/LocationAPIApp.java
@@ -0,0 +1,12 @@
+package rxss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationAPIApp {
+	public static void main(String[] args) {
+		SpringApplication.run(LocationAPIApp.class, args);
+	}
+
+}
